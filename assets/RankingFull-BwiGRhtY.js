688e3e1ba55175f6945eb2fb7774b1bc721e3c03
GearlissonS <silvagearlisson@gmail.com>
2025-08-15 21:44:25 -0300

    Updates

--- assets/RankingFull-BwiGRhtY.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-YHIpdodM.js";import i from"./Ranking-BQeMNLs1.js";import"./index-BybOVMEA.js";import"./index-CNq2K5sw.js";import"./select-Br6BCFxX.js";import"./check-CecYAToQ.js";function e(){return r.jsx("div",{className:"min-h-screen bg-background",children:r.jsx(i,{})})}export{e as default};
