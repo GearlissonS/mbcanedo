dd2309490c17d8790a95cc81d75bfd3263f50aaf
GearlissonS <GearlissonS@users.noreply.github.com>
2025-08-16 19:45:10 +0000

    deploy: 4efe5d8698c7e22b5c3789c0e3ca7f70d1cd55ab

--- assets/RankingFull-ANK4AlId.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-dzNuoPkZ.js";import i from"./Ranking-CT7wmvkM.js";import"./index-DYAaNT93.js";import"./index-C2eC6UDz.js";import"./select-BldK59Bc.js";import"./check-B6QDnmQM.js";function e(){return r.jsx("div",{className:"min-h-screen bg-background",children:r.jsx(i,{})})}export{e as default};
