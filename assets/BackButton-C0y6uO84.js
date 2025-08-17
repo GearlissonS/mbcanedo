28a30227137fd53281bc5db99f086bf67843e4ac
GearlissonS <GearlissonS@users.noreply.github.com>
2025-08-17 02:23:53 +0000

    deploy: a884288f91035edda1d89ff686200b5767be94b4

--- assets/BackButton-C0y6uO84.js
+++
@@ -1,6 +0,0 @@
-import{c as o,w as r,j as t}from"./index-Bv9Bj91-.js";import{B as s}from"./button-B1dnoN7i.js";/**
- * @license lucide-react v0.462.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const n=o("ArrowLeft",[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]]);function m({label:a="Voltar"}){const e=r();return t.jsxs(s,{variant:"outline",className:"mb-4",onClick:()=>e(-1),children:[t.jsx(n,{className:"mr-2 h-4 w-4"})," ",a]})}export{m as B};
